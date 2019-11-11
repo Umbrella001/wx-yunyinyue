048e6dadcdb212d8f266ac2c79db1d1a0321a72d
umbrella <13798972826@163.com>
2019-11-11 17:28:55 +0800

    播放歌曲逻辑与动画

--- cloudfunctions/music/index.js
+++ cloudfunctions/music/index.js
@@ -32,8 +32,14 @@ exports.main = async(event, context) => {
 
   app.router('musiclist', async(ctx, next) => {
     ctx.body = await rp(BASE_URL + '/playlist/detail?id=' + parseInt(event.playlistId))
-    .then((res) => {
-      return JSON.parse(res)
+      .then((res) => {
+        return JSON.parse(res)
+      })
+  })
+
+  app.router('musicUrl', async(ctx, next) => {
+    ctx.body = await rp(BASE_URL + `/song/url?id=${event.musicId}`).then((res) => {
+      return res
     })
   })
 
--- miniprogram/pages/player/player.js
+++ miniprogram/pages/player/player.js
@@ -1,85 +1,135 @@
 // pages/player/player.js
 
 let musiclist = []
-let currentMusic = 0
+let currentMusicIndex = 0
+// 获取全局唯一的背景音频管理器
+const backgroundAudioManager = wx.getBackgroundAudioManager()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    playerInfo:{}
+    picUrl: '',
+    isPlaying: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    this.getMusicInfo()
-    currentMusic = options.index
-    const musicInfo = musiclist[currentMusic]
+  onLoad(options) {
+    currentMusicIndex = options.index
+    musiclist = wx.getStorageSync('musiclist')
+    this._loadMusicDetail(options.musicId)
+  },
+
+  _loadMusicDetail(musicId) {
+    backgroundAudioManager.stop()
+    let musicInfo = musiclist[currentMusicIndex]
+    console.log(musicInfo)
     wx.setNavigationBarTitle({
-      title: musicInfo.name,
+      title: musicInfo.name
     })
+
     this.setData({
-      playerInfo:{
-        plyerImgUrl: musicInfo.al.picUrl
-      }
+      picUrl: musicInfo.al.picUrl,
+      isPlaying: false
     })
-    console.log(this.data.playerInfo.plyerImgUrl)
+
+    wx.showLoading({
+      title: '歌曲载入中',
+    })
+
+    wx.cloud.callFunction({
+      name: 'music',
+      data: {
+        musicId,
+        $url: 'musicUrl'
+      }
+    }).then((res) => {
+      console.log(res)
+      let result = JSON.parse(res.result)
+      console.log(result)
+      backgroundAudioManager.src = result.data[0].url
+      backgroundAudioManager.title = musicInfo.name
+      backgroundAudioManager.coverImgUrl = musicInfo.al.picUrl
+      backgroundAudioManager.singer = musicInfo.ar[0].name
+      backgroundAudioManager.epname = musicInfo.ar.name
+
+      this.setData({
+        isPlaying: true
+      })
+
+      wx.hideLoading()
+    })
   },
 
-  getMusicInfo(){
-    musiclist = wx.getStorageSync('musiclist');
-    console.log(musiclist)
+  togglePlaying() {
+    this.data.isPlaying ? backgroundAudioManager.pause() : backgroundAudioManager.play()
+
+    this.setData({
+      isPlaying: !this.data.isPlaying
+    })
   },
 
+  prevMusic() {
+    currentMusicIndex--
+    currentMusicIndex = currentMusicIndex < 0 ? musiclist.length - 1 : currentMusicIndex
+    this._loadMusicDetail(musiclist[currentMusicIndex].id)
+  },
+
+  nextMusic() {
+    currentMusicIndex++
+    currentMusicIndex = currentMusicIndex > musiclist.length - 1 ? 0 : currentMusicIndex
+    this._loadMusicDetail(musiclist[currentMusicIndex].id)
+  },
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
