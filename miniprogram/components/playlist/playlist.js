d846e1143a6a71ef351f34f031193011c2a04fa9
umbrella <13798972826@163.com>
2019-11-19 18:15:41 +0800

    音乐模块--歌单列表首页

--- miniprogram/components/playlist/playlist.js
+++ miniprogram/components/playlist/playlist.js
@@ -1,4 +1,3 @@
-// components/playlist/playlist.js
 Component({
   /**
    * 组件的属性列表
@@ -9,6 +8,7 @@ Component({
     }
   },
 
+  // 监听计算歌单右上角播放量 
   observers: {
     ['playlist.playCount'](count) {
       this.setData({
@@ -20,18 +20,22 @@ Component({
    * 组件的初始数据
    */
   data: {
-    _count: 0
+    _count: 0,  // 歌单播放量
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
+
+    // 歌单跳转到歌单的歌曲列表页面
     goToMusicList(){
       wx.navigateTo({
         url: `../../pages/musiclist/musiclist?playlistId=${this.properties.playlist.id}`,
       })
     },
+
+    //  格式化歌单播放量数字
     _transformNum(num, point) {
       let numStr = num.toString().split('.')[0];
       if (numStr.length < 6) {
