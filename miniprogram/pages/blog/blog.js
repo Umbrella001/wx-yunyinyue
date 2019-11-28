1f8ba078f12747e3cb62ccbf01ad23488f97f957
umbrella <13798972826@163.com>
2019-11-28 18:34:56 +0800

    小程序我的模块 -- 生成小程序码

--- miniprogram/pages/blog/blog.js
+++ miniprogram/pages/blog/blog.js
@@ -9,7 +9,14 @@ Page({
   data: {
     isShowPopup: false, // 是否显示授权底部弹窗，默认false不显示
     blogList: [], // 存放博客页面的博客列表数据
-    isAuthorize: false // 是否授权
+    isAuthorize: false, // 是否授权
+    blogShareImg: [    "https://hbimg.huabanimg.com/0f2e924f9f24b483a6d31ee780208a20306f2a3a11750-rFPrzG_fw658",
+    "https://hbimg.huabanimg.com/0b95640080a36c0990a811f0f74efb98cb9b512652fc4-6gkC2J_fw658",
+    "https://hbimg.huabanimg.com/05f503e79e197232ac0675f4639cd7459ab4b327bdf37-3HoX31_fw658",
+    "https://hbimg.huabanimg.com/00ae5f42556f724d5a13aa235d106ed4bf509ced4c75e-xSp6Wk_fw658",
+    "https://hbimg.huabanimg.com/1e0006a7fe87dd2a5bc229b791c1578a56976c98e4f8c-yPYjCH_fw658",
+    "https://hbimg.huabanimg.com/50e9edef431897c9326f852c4ace4be02c2011a815ecd7-3y7BhI_fw658"
+    ]
   },
 
   /**
@@ -136,9 +143,12 @@ Page({
    */
   onShareAppMessage: function(event) {
     let blogObj = event.target.dataset.blog
+    const len = this.data.blogShareImg.length;
+    let random = Math.floor(Math.random()*len)
     return {
       title: blogObj.content,
-      path: `/pages/blog-comment/blog-comment?blogId=${blogObj._id}`
+      path: `/pages/blog-comment/blog-comment?blogId=${blogObj._id}`,
+      imageUrl: this.data.blogShareImg[random]
     }
   }
 })
